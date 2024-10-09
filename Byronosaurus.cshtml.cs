f7dcd3c7fee4692f8795b67013f891a39eba85a8
jr23ps <112111494+jr23ps@users.noreply.github.com>
2024-10-09 13:58:23 -0500

    Add files via upload

---
+++ Byronosaurus.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project.Pages
+{
+    public class ByronosaurusModel : PageModel
+    {
+        public string question1 { get; set; }
+        public string question2 { get; set; }
+        public string question3 { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
